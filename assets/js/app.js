e87645f97c4e6db938ed50fb1d728ac67a40a261
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-10 15:48:03 +0100

    js

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+
+let one = document.querySelector('#one');
+console.log(one)
+
+one.style.color = '#F00';
+
+let two = document.querySelector('#two');
+console.log(two)
+
+two.style.color = '#F00';
+
+let three = document.querySelector('#three');
+console.log(three)
+
+three.style.color = '#F00';
+
+let four = document.querySelector('#four');
+console.log(four)
+
+four.style.color = '#F00';
+
+let five = document.querySelector('#five');
+console.log(five)
+
+five.style.color = '#F00';
