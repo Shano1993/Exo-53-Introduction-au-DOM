aaa3043d305b36769d94ed209b50b7d01d11b8ad
Shano1993 <stefan.hanotiau@gmail.com>
2021-11-14 13:31:02 +0100

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+/*
+    Paragraphe
+ */
 
 let one = document.querySelector('#one');
 
@@ -8,6 +11,7 @@ let two = document.querySelector('#two');
 
 two.style.color = '#F00';
 two.style.width = '40%';
+two.className = 'animation'
 
 let three = document.querySelector('#three');
 
@@ -18,53 +22,89 @@ let four = document.querySelector('#four');
 
 four.style.color = '#F00';
 four.style.width = '80%';
+four.className = 'animation'
 
 let five = document.querySelector('#five');
 
 five.style.color = '#F00';
 five.style.width = '100%';
+five.innerText = "Mon nouveau texte"
 
-
+/*
+    Span
+ */
 
 let spanOne = document.querySelector("#sone")
 
 spanOne.style.background = '#a00545'
+spanOne.dir = "rtl"
 
 let spanTwo = document.querySelector("#stwo")
 
 spanTwo.style.background = '#a00545'
+spanTwo.dir = "rtl"
 
 let spanThree = document.querySelector("#sthree")
 
 spanThree.style.background = '#a00545'
+spanThree.dir = "rtl"
 
 let spanFour = document.querySelector("#sfour")
 
 spanFour.style.background = '#a00545'
+spanFour.dir = "rtl"
 
 let spanFive = document.querySelector("#sfive")
 
 spanFive.style.background = '#a00545'
+spanFive.dir = "rtl"
 
 let spanSix = document.querySelector("#ssix")
 
 spanSix.style.background = '#a00545'
+spanSix.dir = "rtl"
 
 let spanSeven = document.querySelector("#sseven")
 
 spanSeven.style.background = '#a00545'
+spanSeven.dir = "rtl"
 
 let spanEight = document.querySelector("#seight")
 
 spanEight.style.background = '#a00545'
+spanEight.dir = "rtl"
 
 let spanNine = document.querySelector("#snine")
 
 spanNine.style.background = '#a00545'
+spanNine.dir = "rtl"
 
 let spanTen = document.querySelector("#sten")
 
 spanTen.style.background = '#a00545'
+spanTen.dir = "rtl"
 
+/*
+    Lien
+ */
 
+let aOne = document.querySelector("#a_one")
+
+aOne.innerText = "Lien 1"
+
+let aTwo = document.querySelector("#a_two")
+
+aTwo.innerText = "Lien 2"
+
+let aThree = document.querySelector("#a_three")
+
+aThree.innerText = "Lien 3"
+
+let aFour = document.querySelector("#a_four")
+
+aFour.innerText = "Lien 4"
+
+let aFive = document.querySelector("#a_five")
+
+aFive.innerText = "Lien 5"
 
